07664fc6f50be730f70844d83a8eaaec53dd334a
hdluu96 <hoa.luu@my.tccd.edu>
2018-08-22 18:27:17 -0500

    HelloWorld First Program C#

---
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int x = 10;
+            double y = 3.14;
+            Console.WriteLine("Hello World");
+        }
+    }
+}
