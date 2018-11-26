a0446481904cd6b7941cf3eee5e32039f91d86d2
HOA LUU <hoa.luu@my.tccd.edu>
2018-11-25 19:46:00 -0600

    lab 4 nile?

---
+++ Labs/Nile/Nile.Windows/AboutForm.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Hoa Luu
+ * ITSE 1430
+ * Lab 4
+ * 11/25/2018 
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace Nile.Windows
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOkClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
